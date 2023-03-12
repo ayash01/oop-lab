f0e0acaedf03385f71c56dd34d645461d291677a
ayash01 <2231@tkmce.ac.in>
2023-03-12 18:19:50 +0530

    minor changes

--- co1/1-product.java
+++ co1/1-product.java
@@ -21,53 +21,28 @@ class q1 {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter name, product code, and price of product 1: ");
-        pname[0] = input.nextLine();
-        pcode[0] = Integer.parseInt(input.nextLine());
-        price[0] = Integer.parseInt(input.nextLine());
-
-        System.out.println("\nEnter name, product code, and price of product 2: ");
-        pname[1] = input.nextLine();
-        pcode[1] = Integer.parseInt(input.nextLine());
-        price[1] = Integer.parseInt(input.nextLine());
-
-        System.out.println("\nEnter name, product code, and price of product 3: ");
-        pname[2] = input.nextLine();
-        pcode[2] = Integer.parseInt(input.nextLine());
-        price[2] = Integer.parseInt(input.nextLine());
-
-        Product product1 = new Product(pname[0], pcode[0], price[0]);
-        Product product2 = new Product(pname[1], pcode[1], price[1]);
-        Product product3 = new Product(pname[2], pcode[2], price[2]);
-
-        if (product1.price < product2.price) {
-            if (product1.price < product3.price) {
-                System.out.println("Product 1 has the lowest price (" + price[0] + ")");
-            }
-            else if (product1.price == product3.price) {
-                System.out.println("Product 1 & 3 have the lowest price");
-            }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter name, product code, and price of product " + (i+1) + ": ");
+            pname[i] = input.nextLine();
+            pcode[i] = Integer.parseInt(input.nextLine());
+            price[i] = Integer.parseInt(input.nextLine());
         }
 
-        /*
-        else if (product2.price < product1.price && product2.price < product3.price) {
-            System.out.println("Product 2 has the lowest price (" + price[1] + ")");
+        Product p1 = new Product(pname[0], pcode[0], price[0]);
+        Product p2 = new Product(pname[1], pcode[1], price[1]);
+        Product p3 = new Product(pname[2], pcode[2], price[2]);
+
+        if (p1.price < p2.price && p1.price < p3.price) {
+                System.out.println(pname[0] + " has the lowest price (" + p1.price + ")");
         }
-        else if (product3.price < product1.price && product3.price < product2.price) {
-            System.out.println("Product 3 has the lowest price (" + price[2] + ")");
+        else if (p2.price < p1.price && p2.price < p3.price) {
+            System.out.println(pname[1] + " has the lowest price (" + p2.price + ")");
         }
-        else if (product1.price == product2.price && product1.price < product1.price) {
-            System.out.println("Product 1 & 2 have the lowest price");
-        }
-        else if (price[1] == price[2] && price[1] < price[0]) {
-            System.out.println("Product 2 & 3 have the lowest price");
-        }
-        else if (price[0] == price[2] && price[0] < price[1]) {
-            System.out.println("Product 1 & 3 have the lowest price");
+        else if (p3.price < p1.price && p3.price < p2.price) {
+            System.out.println(pname[2] + " has the lowest price (" + p3.price + ")");
         }
         else {
             System.out.println("All products have the same price");
         }
-        */
     }
 }
