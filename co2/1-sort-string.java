d28203b6a3189e1cb68b00914d27fe9863a633b0
ayash01 <2231@tkmce.ac.in>
2023-03-12 18:00:34 +0530

    add co2

---
+++ co2/1-sort-string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class sort_string {
+    public static String sort(String string) {
+        char[] array = string.toCharArray();
+        Arrays.sort(array);
+        return new String(array);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input_str = input.nextLine();
+        System.out.println("The sorted string is " + sort(input_str));
+    }
+}
