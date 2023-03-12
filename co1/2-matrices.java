f77298576840428990d7813400a61c848c9df39d
ayash01 <2231@tkmce.ac.in>
2023-03-12 16:04:56 +0530

    minor changes

--- co1/2-matrices.java
+++ co1/2-matrices.java
@@ -1,5 +1,44 @@
 import java.util.Scanner;
 
+class Matrix {
+    private final int[][] matrix;
+
+    public Matrix(int rows, int cols) {
+        matrix = new int[rows][cols];
+    }
+
+    public void setElement(int row, int col, int value) {
+        matrix[row][col] = value;
+    }
+
+    public int getElement(int row, int col) {
+        return matrix[row][col];
+    }
+
+    public void printMatrix() {
+        for (int[] ints : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static Matrix defineMatrix(int rows, int cols) {
+        Scanner input = new Scanner(System.in);
+        Matrix matrix = new Matrix(rows, cols);
+
+        System.out.println("\nEnter elements of matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix.setElement(i, j, input.nextInt());
+            }
+        }
+
+        return matrix;
+    }
+}
+
 class q2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -9,52 +48,24 @@ class q2 {
         System.out.println("Enter number of columns in matrices:");
         int col = Integer.parseInt(input.nextLine());
 
-        int[][] a = new int[row][col];
-        int[][] b = new int[row][col];
-        int[][] c = new int[row][col];
-
-        System.out.println("\nEnter elements of first matrix:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                a[i][j] = Integer.parseInt(input.nextLine());
-            }
-        }
-
-        System.out.println("\nEnter elements of second matrix:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                b[i][j] = Integer.parseInt(input.nextLine());
-            }
-        }
+        Matrix a = Matrix.defineMatrix(row, col);
+        Matrix b = Matrix.defineMatrix(row, col);
 
         System.out.println("\nMatrix 1:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(a[i][j] + "  ");
-            }
-            System.out.println("");
-        }
+        a.printMatrix();
 
         System.out.println("\nMatrix 2:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(b[i][j] + "  ");
-            }
-            System.out.println("");
-        }
+        b.printMatrix();
+
+        Matrix c = new Matrix(row, col);
 
         for (int i =0; i < row; i++) {
-            for (int j = 0; j < row; j++) {
-                c[i][j] = a[i][j] + b[i][j];
+            for (int j = 0; j < col; j++) {
+                c.setElement(i, j, a.getElement(i, j) + b.getElement(i, j));
             }
         }
 
         System.out.println("\nSum of Matrices:");
-        for (int i =0; i < row; i++) {
-            for (int j = 0; j < row; j++) {
-                System.out.print(c[i][j] + "  ");
-            }
-            System.out.println("");
-        }
+        c.printMatrix();
     }
 }
