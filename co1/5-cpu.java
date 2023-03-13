06376ec70e92aed6ee807c158ef867f0e1800219
ayash01 <2231@tkmce.ac.in>
2023-03-13 06:01:57 +0530

    minor changes

--- co1/5-cpu.java
+++ co1/5-cpu.java
@@ -21,28 +21,23 @@ class q5 {
         CPU cpu = new CPU();
 
         System.out.println("Enter price of the CPU");
-        int price = Integer.parseInt(input.nextLine());
-        cpu.price = price;
+        cpu.price = Integer.parseInt(input.nextLine());
 
         CPU.Processor processor = cpu.new Processor();
 
         System.out.println("Enter number of cores: ");
-        int cores = Integer.parseInt(input.nextLine());
-        processor.cores = cores;
+        processor.cores = Integer.parseInt(input.nextLine());
 
         System.out.println("Enter processor manufacturer: ");
-        String manufacturer = input.nextLine();
-        processor.manufacturer = manufacturer;
+        processor.manufacturer = input.nextLine();
 
         CPU.Processor.RAM ram = new CPU.Processor.RAM();
 
         System.out.println("Enter RAM manufacturer: ");
-        String ram_manufacturer = input.nextLine();
-        ram.manufacturer = ram_manufacturer;
+        ram.manufacturer = input.nextLine();
 
         System.out.println("Enter size of RAM (in GB): ");
-        int memory = Integer.parseInt(input.nextLine());
-        ram.memory = memory;
+        ram.memory = Integer.parseInt(input.nextLine());
 
         System.out.println("\nCPU Price: "+cpu.price+"\nProcessor cores: "+processor.cores+"\nProcessor manufacturer: "+processor.manufacturer+"\nRAM manufacturer: "+ram.manufacturer+"\nRAM size: "+ram.memory+"GB");
     }
